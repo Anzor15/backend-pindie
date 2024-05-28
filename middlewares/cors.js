ffe0ed9a3de207748875a7570111d5efe7974f7d
Anzor15 <150362340+Anzor15@users.noreply.github.com>
2024-05-28 17:59:07 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,8 +5,6 @@ function cors(req, res, next) {
         res.header('Access-Control-Allow-Origin', origin);
        
     }
-     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
      res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
@@ -17,7 +15,7 @@ const allowedCors = [
   "https://students-projects.ru",
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://anzori-tsemi.nomorepartiesco.ru"
+  "https://anzori-tsemi.nomorepartiesco.ru",
 ];
 
   
